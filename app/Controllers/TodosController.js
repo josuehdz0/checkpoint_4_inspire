3bb9fa19ab7282ded83ebd2dc2e8e0ef1fb33fbc
Josue Hernandez <josuehdz0@gmail.com>
2023-02-10 13:51:26 -0700

    layout for todolist started

---
+++ app/Controllers/TodosController.js
@@ -0,0 +1,5 @@
+export class TodosController{
+  constructor(){
+    console.log('todo controller?');
+  }
+}
---
+++ app/Models/Todo.js
@@ -0,0 +1,10 @@
+export class Todo{
+
+  constructor(data){
+    this.completed = data.completed
+    this.description = data.description
+    this.user = data.user
+  }
+
+  
+}
