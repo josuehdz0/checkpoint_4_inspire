8a75e9f60b7f3ef134dd426627cd930489a1b672
Josue Hernandez <josuehdz0@gmail.com>
2023-02-11 15:51:42 -0700

    WEATHER WORKS! toggle between F and C

--- app/Models/Weather.js
+++ app/Models/Weather.js
@@ -3,20 +3,34 @@ export class Weather{
   constructor(data){
     this.name = data.name
     this.main = data.main.temp
-    this.celcius = ((data.main.temp)-273).toFixed(1)
+    this.celsius = ((data.main.temp)-273).toFixed(1)
     this.fahrenheit = (1.8*(data.main.temp-273.15)+32).toFixed(1)
     this.weather = data.weather
+    this.isFahrenheit = true;
   }
 
 
   get temperatureTemplate(){
-    return`
-    <button class="btn btn-light">
-    <h4>${this.fahrenheit}<i class="mdi mdi-temperature-fahrenheit"></i></h4>
-  </button>
-    `
 
-    
-    
+    // NOTE this just shows how to print one temp at a time
+  //   return`
+  //   <button class="btn btn-light">
+  //   <h4>${this.fahrenheit}<i class="mdi mdi-temperature-fahrenheit"></i></h4>
+  // </button>
+  //   `
+
+
+  // NOTE this ask if temp is fahrenheit or not, does the same with icon.
+  const temperature = this.isFahrenheit ? this.fahrenheit : this.celsius;
+  const icon = this.isFahrenheit ? 'mdi-temperature-fahrenheit' : 'mdi-temperature-celsius';
+
+  // NOTE here is were the function dynamically switches between variables above
+  return`
+  <button class="btn btn-light"  onclick="app.weatherController.toggleTemperatureUnit()">
+    <h4>${temperature}<i class="mdi ${icon}"></i></h4>
+  </button>
+  `
   }
+
+ 
 }
